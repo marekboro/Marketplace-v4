0019e8e99cd079828618122a22245bc6dba6a0f6
Marek B <mareboro@o2.pl>
2022-01-29 17:57:05 +0000

    Added a model for  Product

---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Marketplace_v4.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Price    { get; set; }
+
+        public Product(int id, string name, int price)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+        }
+     }
+}
